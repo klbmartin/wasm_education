8bc2e2aaf27750900993be223c95d99c0b3788af
Martin Kolb <klb.martin@gmail.com>
2021-04-09 19:58:31 +0200

    Initial commit

---
+++ copy.c
@@ -0,0 +1,10 @@
+#include <string.h>
+
+int copy(char *input, char *output)
+{
+    const int length = strlen(input);
+
+    strncpy(output, input, length);
+
+    return length;
+}
---
+++ make_malloc_copy.sh
@@ -0,0 +1,11 @@
+export PATH=/usr/local/opt/llvm/bin:$PATH
+#clang -v --target=wasm32-unknown-wasi --sysroot /tmp/wasi-libc -nostartfiles -Wl,--import-memory -Wl,--no-entry -Wl,--export-all -o copy.wasm copy.c
+clang \
+--target=wasm32-unknown-wasi \
+--sysroot /tmp/wasi-libc \
+-nostartfiles \
+-Wl,--import-memory \
+-Wl,--no-entry \
+-Wl,--export-all \
+-o malloc_copy.wasm \
+malloc_copy.c
